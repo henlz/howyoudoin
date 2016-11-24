fef4686c6ab8189f96d63bd4e615f34024214d5c
Leticia Portella <leportella@gmail.com>
2016-11-24 10:04:17 -0200

    added geeks and pokemon hitlines

--- helpme.py
+++ helpme.py
@@ -1,8 +1,13 @@
 from json import loads
 from random import choice
 
-def catchthemall():
-    file = open("./cantadas.json", 'r')
+def catchthemall(pokemon=False, nerds=False):
+    if pokemon:
+        file = open("./pokemon.json", 'r')
+    elif nerds:
+        file = open("./nerds.json", 'r')
+    else:
+        file = open("./cantadas.json", 'r')
     string = str(file.read())
     cantadas = loads(string)
     result = choice(cantadas)
