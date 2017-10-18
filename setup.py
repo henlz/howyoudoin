c99f58350027a24140f479e2eb0236f37b5d48cc
Henrique Lobato Zago <lobato.zago@gmail.com>
2017-10-18 14:09:10 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='howyoudoin',
     version='alpha-0.1',
     description='Have you ever needed more than How you doing?',
-    url='https://github.com/leportella/howyoudoin',
-    author='leportella e guidiego',
+    url='https://github.com/henlz/howyoudoin',
+    author='leportella, guidiego and henlz',
     license='MIT',
     packages=['howyoudoin']
 )
